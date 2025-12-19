a30f68f7000b9ae7fb48629e1a7f07d422cc0811
Nicolai Schou <nisc0003@stud.kea.dk>
2025-12-19 09:37:01 +0100

    synced password

--- src/assets/login.js
+++ src/assets/login.js
@@ -85,7 +85,6 @@ class Login extends HTMLElement {
         <div class="loginmodal-container">
           <h1>Login</h1><br>
           <p>This is a school project</p>
-          <p>The password is <code>${this.requiredCode}</code></p>
           <form>
             <input type="password" name="pass" placeholder="Password">
             <input type="submit" name="login" class="login loginmodal-submit" value="Login">
