2ac42849fec36fd0d9367094f5e358829f032ee6
Nicolai Schou <nisc0003@stud.kea.dk>
2025-12-19 08:30:52 +0100

    password_added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
